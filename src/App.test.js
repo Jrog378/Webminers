a79ee94349b877c3fead1d22d7ba76ce57582df3
Jrog378 <Jayjust378@gmail.com>
2022-09-30 22:22:05 -0400

    Fully finished the Front End, Next up Backend and finally we will do web design. Trying to get GH pages to work

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './pages/App';
+import {render, screen} from '@testing-library/react';
+import Homeindex from "./pages/homeindex";
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    render(<Homeindex/>);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
 });
