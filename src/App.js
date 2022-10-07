ad88b3840bb5f27964fceb0c765183fcba711d41
Jrog378 <Jayjust378@gmail.com>
2022-10-07 16:37:00 -0400

    Fully updated Build Folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './stylesheets/index.css';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Homepage from "./pages/homeindex";
 import Homeindex from "./pages/homeindex";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import ErrorPage from "./pages/error-page";
@@ -81,13 +79,6 @@ root.render(
     <React.StrictMode>
         <RouterProvider router={pages}/>
     </React.StrictMode>,
-    <React.StrictMode>
-        <Homepage/>
-    </React.StrictMode>
 );
 
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default root;
