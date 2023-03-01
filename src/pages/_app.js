19b3a44fc8547edfc6e8a56b328c4b1702ce16ce
Justin Rogers <Jayjust378@gmail.com>
2023-02-28 21:42:36 -0500

    Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,12 +3,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from '../components/layout';
 import {Figtree} from "@next/font/google";
 import {SSRProvider} from "react-bootstrap";
+import Script from "next/script";
 
 const Font = Figtree({subsets: ['latin']})
 
 export default function App({Component, pageProps}) {
     return (
         <SSRProvider>
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-38KM92RY3N"
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+    
+              gtag('config', 'G-38KM92RY3N');
+            `}
+            </Script>
             <main className={Font.className}>
                 <Layout>
                     <Component {...pageProps} />
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,24 +1,8 @@
-import {Html, Head, Main, NextScript} from 'next/document'
-import Script from "next/script";
+import {Html, Main, NextScript} from 'next/document'
 
 export default function Document() {
     return (
         <Html lang="en">
-            <Head>
-                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-38KM92RY3N"/>
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-38KM92RY3N', {
-              page_path: window.location.pathname,
-            });
-          `,
-                    }}/>
-
-            </Head>
             <body>
             <Main/>
             <NextScript/>
