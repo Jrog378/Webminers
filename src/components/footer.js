f700c4a8244b9f45035f70d9a06c92f8a4d59a83
Justin Rogers <Jayjust378@gmail.com>
2023-03-29 11:19:32 -0400

    Fixed minified js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer style={{color: 'white', textAlign: 'center', padding:'10px', backgroundColor: '#212529'}}>
-            <p>&copy; <span>{new Date().getFullYear()}</span> Webminers</p>
+            <p>&copy; 2023 Webminers</p>
         </footer>
     );
 }
