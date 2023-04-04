e86482075833b4f1b1423d9a8ca92dcc1f465051
Justin Rogers <Jayjust378@gmail.com>
2023-04-04 19:02:43 -0400

    Setup GA4 with ReactGA library

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,26 +3,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from '../components/layout';
 import {Figtree} from "@next/font/google";
 import {SSRProvider} from "react-bootstrap";
-import Script from "next/script";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { initGA, logPageView } from '@/utils/gtag';
 
 const Font = Figtree({subsets: ['latin']})
 
 export default function App({Component, pageProps}) {
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!window.GA_INITIALIZED) {
+            initGA('G-38KM92RY3N');
+            window.GA_INITIALIZED = true;
+        }
+        logPageView();
+    }, [router.asPath]);
+
     return (
         <SSRProvider>
-            <Script
-                src="https://www.googletagmanager.com/gtag/js?id=G-38KM92RY3N"
-                strategy="lazyOnload"
-            />
-            <Script id="google-analytics" strategy="lazyOnload">
-                {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){window.dataLayer.push(arguments);}
-              gtag('js', new Date());
-    
-              gtag('config', 'G-38KM92RY3N');
-            `}
-            </Script>
             <main className={Font.className}>
                 <Layout>
                     <Component {...pageProps} />
