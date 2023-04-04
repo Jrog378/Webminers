cedbcc26dd03e98a9bddb58d6839506ef8075fee
Justin Rogers <Jayjust378@gmail.com>
2023-04-04 13:53:32 -0400

    Fixed Ads

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,24 +6,13 @@ export default function Document() {
         <Html lang="en">
             <Head>
                 <script
+                    id={'google-ads'}
                     data-ad-client="7878345029704986"
                     async
                     src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
                 ></script>
             </Head>
             <body>
-            <Script
-                id="google-auto-ads"
-                dangerouslySetInnerHTML={{
-                    __html: `
-            (adsbygoogle = window.adsbygoogle || []).push({
-              google_ad_client: "7878345029704986",
-              enable_page_level_ads: true
-            });
-          `,
-                }}
-                strategy={'lazyOnload'}
-            />
             <Main/>
             <NextScript/>
             </body>
