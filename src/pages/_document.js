0f1bc97da59cde0701b096f1fdd3a06bac69b2e4
Justin Rogers <Jayjust378@gmail.com>
2023-04-04 12:03:08 -0400

    Fixed Ads

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -22,6 +22,7 @@ export default function Document() {
             });
           `,
                 }}
+                strategy={'lazyOnload'}
             />
             <Main/>
             <NextScript/>
