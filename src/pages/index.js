29288d92d35c4f190ee3b71e56e395f37edcda04
Justin Rogers <Jayjust378@gmail.com>
2023-05-14 15:02:06 -0400

    New Homescreen

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,10 @@ import React, {useState} from "react";
 import styles from '@/styles/Home.module.css'
 import {Button, Card, CardGroup, Col, Container, Row, Toast} from "react-bootstrap";
 import Link from "next/link";
-import logo from "@/images/WebminersLogo.webp";
 import axios from "axios";
 import {toast} from "react-toastify";
 import CloseButton from 'react-bootstrap/CloseButton';
+import Suggest from "@/components/suggest";
 
 
 function EmailALert() {
@@ -134,15 +134,53 @@ export default function Home() {
                             </Col>
                         </Row>
                     </Card>
-                    <Container>
+                    <Container style={{paddingTop: '10px'}}>
                         <Row>
+                            <Col lg={7} md={6} sm={12} className={styles.pad}>
+                                <Card className={styles.cardhover}>
+                                    <Card.Body>
+                                        <Card.Title style={{fontSize: 'x-large'}}>
+                                            Maximize Returns with Data Mining Findings
+                                        </Card.Title>
+                                        <Card.Text style={{fontSize: 'larger'}}>
+                                            Webminers Investing provides data mining research backed
+                                            education to help you make informed crypto investing decisions.
+                                            Our mission is to help anyone from those who are just starting out
+                                            with crypto to veteran investors.
+                                        </Card.Text>
+                                        <Card.Text style={{fontSize: 'larger'}}>
+                                            We believe that cryptocurrency is a great option as an investment
+                                            asset and are happy to direct your to our research resources.
+                                            <Link className={styles.weblink} href={'/insights'}> Market
+                                                Insights</Link>,
+                                            <Link className={styles.weblink}
+                                                  href={'/balancing'}> Asset Balancing</Link>,
+                                            <Link className={styles.weblink} href={'/research'}> Asset
+                                                Efficiency</Link>, and <Link className={styles.weblink}
+                                                                             href={'/articles'}> Education </Link>
+                                            can help provide you the knowledge and research to
+                                            build or improve a well rounded investing system with Risk Management,
+                                            Asset Allocation, and Asset Selection.
+                                        </Card.Text>
+                                        <Button variant="success" href='/pricing'>Webminers
+                                            Pricing</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col lg={5} md={6} sm={12} className={styles.pad}>
+                                <Suggest Detail={'/'}/>
+                            </Col>
                             <Col lg={12} className={styles.pad}>
                                 <Container>
-                                    <Card.Title id={'why'} style={{paddingBottom: '15px', fontSize: 'x-large'}}>
+                                    <Card.Title id={'why'} style={{
+                                        paddingBottom: '15px',
+                                        textAlign: 'center',
+                                        fontSize: 'x-large'
+                                    }}>
                                         Missing out on opportunities to grow your wealth from not having
                                         the right tools or knowledge?
                                     </Card.Title>
-                                    <p style={{fontSize: 'larger'}}>
+                                    <p style={{fontSize: 'large'}}>
                                         If you want efficient profits and to beat the market, Webminers is where to be.
                                         We
                                         specialize in identifying the ups and downs of the crypto market and identifying
@@ -156,75 +194,35 @@ export default function Home() {
                             <Col lg={12} className={styles.pad}>
                                 <Card className={styles.cardhover}>
                                     <Card.Body>
-                                        <Card.Title style={{width: '90%', textAlign: 'center'}}>
-                                            Let Webminers help out by giving you all the research and education
-                                            you need
-                                            to be successful in the crypto market
+                                        <Card.Title style={{textAlign: 'center', fontSize: 'x-large'}}>
+                                            Giving you all the research and education you need to be successful in the
+                                            crypto market
                                         </Card.Title>
-                                        <ul>
-                                            <li>
-                                                Learn to build an Investing System that produces consistent
-                                                returns making it,
-                                                clear and concise to grow your wealth.
-                                            </li>
-                                            <li>
-                                                Beat the market by outperforming top assets like BTC and the S&P
-                                                500
-                                                with efficient investing from an systems similar to our already
-                                                achieven result.
-                                            </li>
-                                            <li>
-                                                Reduce risk by investing in the right assets based on data
-                                                mining
-                                                findings with maintaining and rebalancing your portfolio
-                                                accordingly.
-                                            </li>
-                                            <li>
-                                                Follow along with easy-to-understand research that is
-                                                simple to use to your advantage
-                                            </li>
-                                        </ul>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                            <Col lg={6} md={12} sm={12} className={styles.pad}>
-                                <Card className={styles.cardhover}>
-                                    <Card.Body>
-                                        <Card.Title>
-                                            Maximize Returns with Data Mining Findings
-                                        </Card.Title>
-                                        <Card.Text>
-                                            Webminers Investing provides data mining research backed
-                                            education to help you make informed crypto investing decisions with
-                                            <Link className={styles.weblink} href={'/insights'}> Market
-                                                Insights</Link>,
-                                            <Link className={styles.weblink}
-                                                  href={'/balancing'}> Asset Balancing</Link>, and
-                                            <Link className={styles.weblink} href={'/research'}> Asset
-                                                Efficiency</Link>. These factors can help build a well rounded
-                                            investing system with Risk Management, Asset Allocation, and Asset
-                                            Selection.
-                                        </Card.Text>
-                                        <Button variant="success" href='/pricing'>Webminers
-                                            Pricing</Button>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                            <Col lg={6} md={12} sm={12} className={styles.pad}>
-                                <Card className={styles.cardhover}>
-                                    <Card.Body>
-                                        <Card.Title>
-                                            Learn to Unlock Your Investing Potential
-                                        </Card.Title>
-                                        <Card.Text>
-                                            There are infinite ways to invest, and building your investing
-                                            system can be tricky. We are here to help. Whether you are just
-                                            starting or have been investing for years, we have educational
-                                            content for you. We want to help crypto investors become more
-                                            informed and confident in their systems to achieve their
-                                            financial goals.
-                                        </Card.Text>
-                                        <Button variant="success" href='/articles'>Educational Content</Button>
+                                        <p style={{fontSize: 'large'}}>
+                                            <ul>
+                                                <li>
+                                                    Learn to build an Investing System that produces consistent
+                                                    returns making it,
+                                                    clear and concise to grow your wealth.
+                                                </li>
+                                                <li>
+                                                    Beat the market by outperforming top assets like BTC and the S&P
+                                                    500
+                                                    with efficient investing from an systems similar to our already
+                                                    achieven result.
+                                                </li>
+                                                <li>
+                                                    Reduce risk by investing in the right assets based on data
+                                                    mining
+                                                    findings with maintaining and rebalancing your portfolio
+                                                    accordingly.
+                                                </li>
+                                                <li>
+                                                    Follow along with easy-to-understand research that is
+                                                    simple to use to your advantage
+                                                </li>
+                                            </ul>
+                                        </p>
                                     </Card.Body>
                                 </Card>
                             </Col>
@@ -311,18 +309,14 @@ export default function Home() {
                                         <Card style={{backgroundColor: 'rgba(0,0,0,0)'}}>
                                             <Card.Body>
                                                 <Card.Text>
-                                                    Take a look at our articles you will find
-                                                    interesting
-                                                    topics,
-                                                    updates, and even just content about webminers like
-                                                    our
-                                                    history
-                                                    and plans for the future.
+                                                    Join our email list for important updates about webminers services.
+                                                    There you will get updates about articles and any important changes
+                                                    to our Crypto Investing system
                                                 </Card.Text>
-                                                <Button href='/articles'
+                                                <Button href='/email-list'
                                                         style={{float: 'left', margin: '5px'}}
                                                         variant="outline-dark"
-                                                        size='md'>Articles</Button>
+                                                        size='md'>Email List</Button>
                                             </Card.Body>
                                         </Card>
                                     </Col>
