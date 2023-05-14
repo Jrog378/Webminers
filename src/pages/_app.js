f6f5090e050805468b64e22259b4c5ea9cd0ac10
Justin Rogers <Jayjust378@gmail.com>
2023-05-13 23:05:05 -0400

    Added Google Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,12 +4,14 @@ import {Figtree} from "@next/font/google";
 import {SSRProvider} from "react-bootstrap";
 import Navbar from '../components/navbar'
 import Footer from "@/components/footer";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 const Font = Figtree({subsets: ['latin']})
 
 export default function App({Component, pageProps}) {
     return (
         <SSRProvider>
+            <GoogleAnalytics gaMeasurementId={'G-38KM92RY3N'}/>
             <main className={Font.className}>
                 <Navbar/>
                 <Component {...pageProps} />
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,8 @@
 import {Html, Head, Main, NextScript} from 'next/document'
-import Script from "next/script";
 
 export default function Document() {
     return (
         <Html lang="en">
-            <Script src="https://www.googletagmanager.com/gtag/js?id=G-38KM92RY3N" async defer/>
             <Head>
             </Head>
             <body>
