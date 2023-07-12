6242576bc06bec7913a5c5c3a343dc9de3ac3b9d
Justin Rogers <Jayjust378@gmail.com>
2023-07-12 07:15:00 -0400

    Grammarly Edit

--- src/pages/articles/ethical-hacking.js
+++ src/pages/articles/ethical-hacking.js
@@ -14,53 +14,53 @@ export default function EthicalHacking() {
             title: '',
             id: 'Intro',
             description: 'MacBook Pro Laptop with two credit cards resting on the keyboard with a padlock on top of those.',
-            text: 'Crypto investors can have entire life savings online at a given time that they are unable to afford to lose, then crypto hackers come along. Mercilessly taking everything. The crypto industry is revolutionary for their technology and decentralization, but there are still some drawbacks. Hackers have become an issue recently in the crypto world.\n' +
+            text: 'Crypto investors can have entire life savings online at a given time that they cannot afford to lose, then crypto hackers come along. Mercilessly taking everything. The crypto industry is revolutionary for their technology and decentralization, but some drawbacks remain. Hackers have become an issue recently in the crypto world.\n' +
                 '\n' +
-                'There is still hope though, here are some ways that you can keep yourself safe from crypto hackers:\n' +
+                'There is still hope, though. Here are some ways that you can keep yourself safe from crypto hackers:\n' +
                 '\n' +
-                '1. [Safety Basics](#Basics)\n' +
-                '2. [Crypto Vulnerabilities](#Vulnerabilities)\n' +
-                '3. [Crypto Device Protections](#DeviceProtections)\n'
+                '[Safety Basics](#Basics)\n' +
+                '[Crypto Vulnerabilities](#Vulnerabilities)\n' +
+                '[Crypto Device Protections](#DeviceProtections)\n'
         },
         {
             title: 'Safety Basics',
             id: 'Basics',
             img: Ethical1,
             description: 'A small section of a Mac Laptop with a rainbow gradient light from red to blue and a black keyboard.',
-            text: 'When talking about ethical hackers and regular hackers, it may get confusing which side they are on. To clarify, Ethical Hackers, also known as white hat hackers, are those who use hacking for ethical purposes to try and find weaknesses and how to fix them. Regular hackers, on the other hand, also known as black hat hackers, find those weaknesses and use them for their good.\n' +
+            text: 'When discussing ethical and regular hackers, it may need clarification on which side they are on. To clarify, Ethical Hackers, also known as white hat hackers, use hacking for ethical purposes to try and find weaknesses and how to fix them. On the other hand, regular hackers, also known as black hat hackers, find those weaknesses and use them for their good.\n' +
                 '\n' +
-                'Just because there are black hat hackers out there does not mean that they are attacking you specifically. They will be going after individuals that take the least amount of work to hack into for the most return. If you are to put up minimal defenses, many hackers would rather just find someone else rather than attempt to hack you. Defenses that you can use are not using public Wifi, using a [VPN](https://www.howtogeek.com/133680/htg-explains-what-is-a-vpn/) on private Wifis, and using built-in virus protection on your devices.\n' +
+                'Just because black hat hackers are out, there does not mean they are attacking you specifically. They will be going after individuals that take the least amount of work to hack into for the most return. If you are to put up minimal defenses, many hackers would just find someone else rather than attempt to hack you. Reasons that you can use are not using public Wifi, [using a VPN](https://www.howtogeek.com/133680/htg-explains-what-is-a-vpn/) on private Wifis, and using built-in virus protection on your devices.\n' +
                 '\n' +
-                'Web safety is also important to take into consideration because even if you have a fully protected computer, you still have to worry about what you willingly give access to your computer. When on the web make sure to only use trusted links or [https, rather than http](https://www.cloudflare.com/learning/ssl/why-is-http-not-secure/) sites. Don’t click links from untrusted emails or text messages. Even trusted contacts sending you a sketchy link should not be fully trusted, just because someone else trusts a link does not mean you have to as well. Trust your instincts.\n'
+                'Web safety is also essential because even if you have a fully protected computer, you still have to worry about what you willingly give access to your computer. Only use trusted links or [https rather than http](https://www.cloudflare.com/learning/ssl/why-is-http-not-secure/) sites on the web. Don’t click links from untrusted emails or text messages. Even trusted contacts sending you a sketchy link should not be fully charged. Just because someone else depends on a link does not mean you have to. Trust your instincts.\n'
         },
         {
             title: 'Crypto Vulnerabilities',
             id: 'Vulnerabilities',
             img: Ethical2,
             description: 'Bitcoin sitting in front of a computer screen that has the crypto market graph that is in the color blue.',
-            text: 'While crypto is known for its safety there are still some vulnerabilities that you have to look out for. The blockchain technology that is used in [crypto transactions](blockchain-transaction) is a peer-to-peer system that is very open to the public including hackers. Your crypto\'s safety will be largely dependent on your actions on your computer as well as how you manage the risk that comes with crypto investing online. Hackers can even make money right from your accounts. \n' +
+            text: 'While crypto is known for its safety, there are still some vulnerabilities that you have to look out for. The blockchain technology used in [crypto transactions](blockchain-transaction) is a peer-to-peer system that is very open to the public, including hackers. Your crypto\'s safety will largely depend on your actions on your computer and how you manage the risk that comes with crypto investing online. Hackers can even make money right from your accounts.\n' +
                 '\n' +
-                'When it comes to your hard-earned money’s safety, not every exchange will be a good fit for you. Some exchanges that you will be forced to use when investing out of the [ordinary altcoins](what-is-an-altcoin) may not be that secure. They can be vulnerable to hacks themselves, risking the money you put into your account with them. The other issue that exchanges may have is internal issues that you should look out for. The [FTX situation](https://www.investopedia.com/what-went-wrong-with-ftx-6828447) is a great example of how much money was stolen from regular crypto investors from trusting a corrupt exchange.\n' +
+                'Only some exchanges regarding your hard-earned money\'s safety will be a good fit for you. Some businesses you will be forced to use when investing out-of-the-ordinary [altcoins](what-is-an-altcoin) may need to be more secure. They can be vulnerable to hacks themselves, risking the money you put into your account with them. The other issue that exchanges may have is internal issues that you should look for. The [FTX situation](https://www.investopedia.com/what-went-wrong-with-ftx-6828447) is an excellent example of how much money was stolen from regular crypto investors from trusting a corrupt exchange.\n' +
                 '\n' +
-                'There are also centralized companies backing certain decentralized cryptocurrencies like Silicon Valley Bank backing USDC. When the bank failed the price of USDC fell almost 20 cents lower than a dollar, which was not supposed to happen for [this stablecoin](are-stablecoins-stable). The background and foundation of crypto can be irregular and cause extra risk when hoping to invest in it. One of the easiest ways to keep yourself safe from these issues is by sticking with a [popular cryptocurrency](popular-cryptocurrency).\n'
+                'Centralized companies also support specific decentralized cryptocurrencies, like Silicon Valley Bank keeping USDC. When the bank failed, the price of USDC fell almost 20 cents lower than a dollar, which was not supposed to happen for [this stablecoin](are-stablecoins-stable). The background and foundation of crypto can be irregular and cause extra risk when hoping to invest in it. One of the easiest ways to keep yourself safe from these issues is by sticking with a [popular cryptocurrency](popular-cryptocurrency).\n'
         },
         {
             title: 'Crypto Device Protections',
             id: 'DeviceProtections',
             img: Ethical3,
             description: 'Computer screen with a mouse hovering over a button that is labeled with the word security in the color blue.',
-            text: 'To keep yourself safe from crypto hacks there is still more that you can do on your side to keep [your crypto wallets](https://www.coinbase.com/learn/crypto-basics/what-is-a-crypto-wallet) safe from harm. The basics like a VPN and no public wifi are still suggested and are a great start to your security, but you are going to need more. Hackers will be persistent when there is a lot of money involved.\n' +
+            text: 'To keep yourself safe from crypto hacks, you can do more to keep [your crypto wallets](https://www.coinbase.com/learn/crypto-basics/what-is-a-crypto-wallet) safe from harm. The basics, like a VPN and no public wifi, are still suggested and are a great start to your security, but you will need more. Hackers will be persistent when there is a lot of money involved.\n' +
                 '\n' +
-                'Do not write your secret phrases on your laptop or iPhone. If one of your devices happens to get hacked you want to make sure that they are not able to do any more harm than just what’s on the phone. These phrases should also not be put in plain sight. If someone were to break into the area where you store your computer they have that phrase right there. It would be a good idea to write it down somewhere you will remember or save it in an encrypted file that you put onto a USB.\n' +
+                'Do not write your secret phrases on your laptop or iPhone. If one of your devices gets hacked, you want to ensure that it cannot do any more harm than just what’s on the phone. These phrases should also not be put in plain sight. If someone were to break into the area where you store your computer, they have that phrase right there. It would be a good idea to write it down somewhere you will remember or save it in an encrypted file you put onto a USB.\n' +
                 '\n' +
-                'Another type of hack is where two companies or partners will make an agreement called a [smart contract](https://www.ibm.com/topics/smart-contracts). This smart contract is on the blockchain and removes the need for a middleman. The money is then put into the blockchain and held there until all terms of the agreement have been met, where the two sides then exchange what was agreed upon. This sounds great and more secure than usual, yet there are still vulnerabilities. If this smart contract is not written well, a hacker can exploit the contract, taking all the cryptocurrency that was supposed to be sent to the worker.\n'
+                'Another type of hack is where two companies or partners will make an agreement called a [smart contract](https://www.ibm.com/topics/smart-contracts). This smart contract is on the blockchain and removes the need for a middleman. The money is then put into the blockchain and held there until all terms of the agreement have been met, where the two sides then exchange what was agreed upon. This sounds great and more secure than usual, yet there are still vulnerabilities. Suppose this intelligent contract needs to be written better. In that case, a hacker can exploit the contract, taking all the cryptocurrency that was supposed to be sent to the worker.\n'
         },
         {
             title: 'Summary',
             img: Ethical4,
             id: 'Summary',
             description: 'Random characters flowing over the screen from top to bottom in the color green with a black background, the matrix.',
-            text: 'Overall there are many methods to protect yourself from the possible outcomes that come with crypto hackers. Ethical hackers are great because they will be able to help regular businesses find the same vulnerabilities that those black hat hackers are looking for. Staying safe takes some time so that you can look into what you are doing. Always remember your VPN and do not use public wifi. Crypto investing is only as safe as you let it be.\n'
+            text: 'Overall there are many methods to protect yourself from the possible outcomes of crypto hackers. Ethical hackers are great because they can help regular businesses find the same vulnerabilities that those black hat hackers are looking for. Staying safe takes some time so that you can look into what you are doing. Always remember your VPN and do not use public wifi. Crypto investing is only as safe as you let it be.\n'
         }
     ]
     const router = useRouter();
--- src/pages/articles/introduction.js
+++ src/pages/articles/introduction.js
@@ -13,20 +13,20 @@ export default function Introduction() {
             title: '',
             id: 'Intro',
             description: 'Man in a black suit suit and a light grey shirt that is reaching out his hand for a greeting handshake.',
-            text: 'As a professional in the crypto scene, I never could have predicted this from such a unique past. I have always had the dream of owning my business and started college in engineering. After analyzing the programs in the engineering department I realized it wasn’t for me. Even though I enjoyed repairing vehicles and machines, I did not feel the passion I was hoping for.\n' +
+            text: 'As a professional in the crypto scene, I could never have predicted this from such a unique past. I have always dreamed of owning my business and started college in engineering. After analyzing the programs in the engineering department, I realized it wasn’t for me. Even though I enjoyed repairing vehicles and machines, I did not feel the passion I hoped for.\n' +
                 '\n' +
-                'I ended up falling into the Data Science major that my college offered. Computer science was on the table but once again, I just didn\'t have the passion for the classes. Data Science felt right to me and I had already taken one of their classes before making the swap.\n' +
+                'I fell into the Data Science major that my college offered. Computer science was on the table, but I needed a passion for the classes. Data Science felt right to me, and I had already taken one of their classes before making the swap.\n' +
                 '\n' +
-                'I started to engulf myself into data, learning data mining and data visualization. I took online classes and worked on extra projects. I researched the jobs and fields I could go into. I was hooked and could not get enough data. I used [kaggle] for datasets and knew that if I wanted to be a professional in the field then my work would have to be similar to those who were competing in coding competitions, but everything looked so complicated.\n'
+                'I started engulfing myself in data, learning data mining and visualization. I took online classes and worked on extra projects. I researched the jobs and fields I could go into. I was hooked and could not get enough data. I used Kaggle for datasets and knew that if I wanted to be a professional in the field, my work would have to be similar to those competing in coding competitions, but everything looked so complicated.\n'
         },
         {
             title: 'Business Goals',
             id: 'BusinessGoals',
             img: Intro1,
             description: 'Many graphs in a mac laptop including, bar chart, map, donut chart, and line graph with a blue theme.',
-            text: 'I wanted to start a business on top of my schooling so I began coding in hopes of learning how to create websites enough to start selling my services. I built my own personal project and worked hard to style it and make it as professional as possible. After I felt I had worked enough, I thought about how much work I just did for a single website and the fact that I did not want to do that for every customer I had. The website I was creating took a lot of effort and specialization, and I knew it was only authentic to me. I felt like I only wanted to develop for myself.\n' +
+            text: 'I wanted to start a business on top of my schooling, so I began coding to learn how to create websites enough to start selling my services. I built my personal project and worked hard to style it and make it as professional as possible. After I felt I had worked enough, I thought about how much work I did for a single website and that I wanted to do something other than that for every customer I had. The website I was creating took a lot of effort and specialization, and I knew it was only authentic to me. I felt like I only wanted to develop for myself.\n' +
                 '\n' +
-                'I needed that unique idea to get me into the life I wanted to live by owning my business. It was summer. I spend time working on my car or doing projects around the house, working with my hands back to my engineer routes. I decided I would attempt a new type of service. I started to research what I could do, finding little things like Data Visualization, Data Storytelling, Data Mining, or even Web Scraping, waiting for the opportunity good enough to start a business.\n' +
+                'I needed that unique idea to get me into the life I wanted to live by owning my business. It was summer. I spend time working on my car or doing projects around the house, working with my hands back to my engineer routes. I decided to attempt a new type of service. I started to research what I could do, finding little things like Data Visualization, Data Storytelling, Data Mining, or even Web Scraping, waiting for the opportunity good enough to start a business.\n' +
                 '\n' +
                 'I also began learning to write professionally because I knew I would need those skills later. Writing is the benchmark to show potential customers who I am and what I can do. Whether it was marketing or understanding how to put attractive text onto websites, technical writing was a must for me to succeed.\n'
         },
@@ -35,16 +35,16 @@ export default function Introduction() {
             id: 'Deadlines',
             img: Intro2,
             description: 'HTML code on a mac laptop that is in the process of being made into a website with different types of tags.',
-            text: 'Once again, the programming side of things did not seem to stick with me. I loved programming and could do it all day as long as I had a purpose. I just had this feeling deep down that programming for others isn\'t my thing. I dropped the idea again. Thinking back, I am grateful for these trials because learning what works for me and what does not, even though it might have seemed like a waste of time, however it has all come back to help me later on.\n' +
+            text: 'Once again, the programming side of things did not stick with me. I loved programming and could do it all day as long as I had a purpose. I just had this feeling deep down that programming for others isn\'t my thing. I dropped the idea again. Thinking back, I am grateful for these trials because learning what works for me and what does not, even though it might have seemed like a waste of time, has all come back to help me later on.\n' +
                 '\n' +
                 'I still had the same issue as before. The more I looked for businesses, the more I felt lost. I bought a 3d Printer to try and create my product and found out quickly how much work would have to go into such a small product where my dollars per hour would be lower than minimum wage. Like everything else, I gathered what I learned and carried it with me.\n' +
                 '\n' +
-                'What to do crowded my mind. Summer was running out, and now was the best time to focus my mind on something and jump-start a business before school began again. I had been a small participant in the Crypto markets with [Coinbase] and their free coins for learning about the company. This time was different. I wanted to use my data science knowledge in the markets.\n'
+                'What to do crowded my mind. Summer was running out, and now was the best time to focus my mind on something and jump-start a business before school began again. I had been a minor participant in the Crypto markets with [Coinbase] and their free coins for learning about the company. This time was different. I wanted to use my data science knowledge in the markets.\n'
         },
         {
-            title: 'The Foundation',
+            title: 'Next Steps',
             img: Intro3,
-            id: 'Foundation',
+            id: 'NextStep',
             description: 'Grey Mac Laptop with a black keyboard with a rainbow gradient light shining down on it from above.',
             text: 'I decided to create a system that could compete with the best in the market and bring me the self-made success I desired so much. I knew it would take a while. So I studied and tested with small amounts. I knew that if I could double my money with $500, I could double any amount.\n' +
                 '\n' +
